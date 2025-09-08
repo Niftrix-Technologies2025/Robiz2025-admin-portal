4c16e8664a18640d94ca241d67c18cb356a3a33a
AshwinSaji10 <aswinsaji777@gmail.com>
2025-09-08 13:55:18 +0530

    created layout for system settings

---
+++ src/pages/system-settings/layouts/SystemSettingsLayout.jsx
@@ -0,0 +1,20 @@
+import TabbedLayout from "../../../components/tabbed-layout/TabbedLayout";
+import { Outlet } from "react-router-dom";
+
+const tabs = [
+    { label: "Modify Schema", url: "/system-settings/modify-schema" },
+    { label: "Manage Policies", url: "/system-settings/manage-policies" },
+];
+
+const SystemSettingsLayout = () => {
+    return (
+        <div className="flex flex-col w-full h-full">
+            <TabbedLayout items={tabs} />
+            <div className="pt-[60px] px-[20px] bg-outletBg w-full h-full">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default SystemSettingsLayout;
