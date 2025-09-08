6883331adc5130a30a22ba7897b32f265bd76d2d
AshwinSaji10 <aswinsaji777@gmail.com>
2025-09-08 13:54:06 +0530

    created basic modal for user activity history

---
+++ src/pages/user-management/components/UserActivityHistorySection.jsx
@@ -0,0 +1,9 @@
+const UserActivityHistorySection = () => {
+  return (
+    <div className="w-full h-full border-[1.5px] border-gray-500 bg-white rounded-[10px] flex-shrink-0">
+        
+    </div>
+  )
+}
+
+export default UserActivityHistorySection
--- src/pages/user-management/modals/UserActivityHistoryModal.jsx
+++ src/pages/user-management/modals/UserActivityHistoryModal.jsx
@@ -1,14 +1,51 @@
 import Modal from "react-modal";
-const UserActivityHistoryModal = ({ isOpen, onRequestClose }) => {
+import { useLoading } from "../../../hooks/useLoading";
+import LoadingItem from "../../../components/LoadingItem";
+import { useEffect } from "react";
+import { fetchUserActivityHistory } from "../../../services/user.service";
+import CustomCloseButton from "../../../components/CustomCloseButton";
+import UserActivityHistorySection from "../components/UserActivityHistorySection";
+const UserActivityHistoryModal = ({ userId, isOpen, onRequestClose }) => {
+    const { isLoading, setIsLoading } = useLoading();
+    useEffect(() => {
+        const fetchActivityHistory = async () => {
+            try {
+                setIsLoading(true);
+                const res = await fetchUserActivityHistory(userId);
+                if (res.status === 200) {
+                    // Handle the fetched activity history data
+                    console.log("User Activity History:", res.data);
+                    setIsLoading(false);
+                }
+            } catch (err) {
+                console.log(err);
+                setIsLoading(false);
+            }
+        };
+        fetchActivityHistory();
+    }, [userId]);
     return (
         <Modal
             isOpen={isOpen}
-            className={`w-[820px] h-[500px] flex flex-col items-center justify-center 
-                rounded-[20px] bg-neumorphicBg p-[20px]`}
+            className={`w-[820px] h-[600px] flex flex-col items-center justify-center 
+                rounded-[20px] bg-neumorphicBg p-[10px]`}
             overlayClassName="modal-overlay"
             onRequestClose={onRequestClose}
             ariaHideApp={false}
-        ></Modal>
+        >
+            <div className="w-full h-full relative">
+                <CustomCloseButton ClickFn={onRequestClose} />
+                {isLoading ? (
+                    <LoadingItem size={10} />
+                ) : (
+                    <div className="flex flex-col w-full h-[90%] mt-[35px] px-[10px] gap-[10px] overflow-y-auto">
+                        <UserActivityHistorySection />
+                        <UserActivityHistorySection />
+                        <UserActivityHistorySection />
+                    </div>
+                )}
+            </div>
+        </Modal>
     );
 };
 
