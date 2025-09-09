dd72d258d5c99d88a2fd18932f0da5f8d9083421
AshwinSaji10 <aswinsaji777@gmail.com>
2025-09-09 15:31:14 +0530

    added functions to call api for add clubs and industry

---
+++ src/services/settings.service.js
@@ -0,0 +1,30 @@
+import { AppConfig } from "../util/AppConfig";
+import axios from "axios";
+
+export const addClub = (
+    districtId,
+    clubName,
+    clubId,
+    zoneName,
+    selectedFile
+) => {
+    const formData = new FormData();
+    formData.append("districtId", districtId);
+    formData.append("clubName", clubName);
+    formData.append("clubId", clubId);
+    formData.append("zoneName", zoneName);
+    formData.append("file", selectedFile);
+    return axios.post(`${AppConfig.api_url}settings/add-club`, formData, {
+        withCredentials: true,
+    });
+};
+
+export const addIndustry = (industry, classification, selectedFile) => {
+    const formData = new FormData();
+    formData.append("industry", industry);
+    formData.append("classification", classification);
+    formData.append("file", selectedFile);
+    return axios.post(`${AppConfig.api_url}settings/add-industry`, formData, {
+        withCredentials: true,
+    });
+};
