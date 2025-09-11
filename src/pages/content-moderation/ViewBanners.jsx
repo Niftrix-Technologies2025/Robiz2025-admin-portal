b69d59c9ab35ccc95e169e9018d1689c0f3bab0b
AshwinSaji10 <aswinsaji777@gmail.com>
2025-09-11 16:52:37 +0530

    displayed list of all banners for content moderation

--- src/pages/content-moderation/ViewBanners.jsx
+++ src/pages/content-moderation/ViewBanners.jsx
@@ -1,11 +1,64 @@
+import { useEffect, useState } from "react";
 import PaginationNavbar from "../../components/PaginationNavbar";
 import { usePagination } from "../../hooks/usePagination";
+import { useLoading } from "../../hooks/useLoading";
+import { fetchAllBanners } from "../../services/content.service";
+import BannerListItem from "./components/BannerListItem";
+import LoadingItem from "../../components/LoadingItem";
+
+const PAGE_SIZE = 100; //100 results per page
 
 const ViewBanners = () => {
+    const { isLoading, setIsLoading } = useLoading();
+    const { page, setPage, total, setTotal, totalPages } =
+        usePagination(PAGE_SIZE);
+    const [banners, setBanners] = useState([]);
+    useEffect(() => {
+        const fetchBanners = async () => {
+            setIsLoading(true);
+            try {
+                const res = await fetchAllBanners({
+                    page,
+                    limit: PAGE_SIZE,
+                });
+                setBanners(res.data.results || []);
+                setTotal(res.data.total || 0);
+                setIsLoading(false);
+                console.log(res.data.results);
+            } catch {
+                console.log("Error fetching banners");
+                setIsLoading(false);
+            }
+        };
+        fetchBanners();
+    }, [page]);
     return (
-        <div className="flex flex-col w-full h-full overflow-y-auto">
-            <PaginationNavbar />
-            <p>he</p>
+        <div className="flex flex-col w-full h-full overflow-y-auto ">
+            <PaginationNavbar
+                pageNo={page}
+                totalPages={totalPages}
+                backwardAction={() => setPage(page - 1)}
+                skipBackwardAction={() => setPage(1)}
+                forwardAction={() => setPage(page + 1)}
+                skipForwardAction={() => setPage(totalPages)}
+            />
+            {isLoading ? (
+                <LoadingItem size={10} />
+            ) : banners.length > 0 ? (
+                <div className="flex flex-col overflow-y-auto pr-[3px] w-full h-full gap-[5px] mb-[20px] mt-[10px]">
+                    {banners.map((banner, index) => (
+                        <BannerListItem
+                            key={index}
+                            index={index}
+                            banner={banner}
+                            page={page}
+                            pageSize={PAGE_SIZE}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div></div>
+            )}
         </div>
     );
 };
---
+++ src/pages/content-moderation/components/BannerListItem.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import ImageDetailModal from "../../../components/modals/ImageDetailModal";
+const columnTitleStyle = "font-medium text-[12px] text-gray-600";
+const BannerListItem = ({ index, banner, page, pageSize }) => {
+    const serialNumber = (page - 1) * pageSize + index + 1;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    return (
+        <div
+            className="flex flex-row justify-between items-start gap-[5px] bg-white rounded-[10px] border-[0.5px] border-gray-200 
+            px-[8px] py-[10px] my-[5px] font-dmSans"
+        >
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>No</p>
+                <p className="w-[40px]">{serialNumber ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>ID</p>
+                <p className="w-[40px]">{banner.userId ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>UserName</p>
+                <p className="w-[120px]">{banner.username ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>BannerType</p>
+                <p className="w-[130px]">{banner.bannerType ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Slot No</p>
+                <p className="w-[30px]">
+                    {banner.trendingBannerSlotNo ?? "-"}
+                </p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Currency</p>
+                <p className="w-[50px]">{banner.currency ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Amount</p>
+                <p className="w-[100px]">{banner.amount ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Creation Date</p>
+                <p className="w-[100px] overflow-x-auto scrollbar-hide">
+                    {new Date(banner.createdAt).toLocaleDateString("en-GB") ??
+                        "-"}
+                </p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Selected Dates</p>
+                <p className="w-[100px] overflow-x-auto scrollbar-hide">
+                    {Array.isArray(banner.selectedDates)
+                        ? banner.selectedDates
+                              .map((dateStr) =>
+                                  new Date(dateStr).toLocaleDateString("en-GB")
+                              )
+                              .join(", ")
+                        : banner.selectedDates ?? "-"}
+                </p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Amount</p>
+                <p className="w-[100px]">{banner.amount ?? "-"}</p>
+            </div>
+            <div className="flex flex-col truncate">
+                <p className={`${columnTitleStyle}`}>Payment Status</p>
+                <p
+                    className={`w-[100px] font-bold ${
+                        banner?.isSuccessful ? "text-green-500" : "text-red-500"
+                    }`}
+                >
+                    {banner?.isSuccessful ? "Success" : "Failure"}
+                </p>
+            </div>
+            <div className="flex flex-col justify-center items-center truncate">
+                <p className={`${columnTitleStyle}`}>Banner Image</p>
+                <img
+                    src={banner.bannerImageUrl}
+                    className={`${
+                        banner?.bannerType == "trending-banner"
+                            ? // ? "w-[52px] h-[80px]"
+                              // : "w-[76px] h-[40px]"
+                              "w-[20px] h-[32px]"
+                            : "w-[38px] h-[20px]"
+                    } cursor-pointer`}
+                    loading="lazy"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default BannerListItem;
