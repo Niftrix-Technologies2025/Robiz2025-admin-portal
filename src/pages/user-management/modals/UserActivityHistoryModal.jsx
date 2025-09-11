7b6d9f7853d372fbaee8c7e80f7620a8ca50a591
AshwinSaji10 <aswinsaji777@gmail.com>
2025-09-11 16:53:58 +0530

    working on new activity history component

--- src/pages/user-management/modals/UserActivityHistoryModal.jsx
+++ src/pages/user-management/modals/UserActivityHistoryModal.jsx
@@ -1,12 +1,21 @@
 import Modal from "react-modal";
 import { useLoading } from "../../../hooks/useLoading";
 import LoadingItem from "../../../components/LoadingItem";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchUserActivityHistory } from "../../../services/user.service";
 import CustomCloseButton from "../../../components/CustomCloseButton";
+import Select from "react-select";
 import UserActivityHistorySection from "../components/UserActivityHistorySection";
+import { IoClose } from "react-icons/io5";
+const dropDownValues = [
+    { value: "premium", label: "Premium Services" },
+    { value: "referral", label: "Referrals" },
+];
 const UserActivityHistoryModal = ({ userId, isOpen, onRequestClose }) => {
     const { isLoading, setIsLoading } = useLoading();
+    const [selectedAttribute, setSelectedAttribute] = useState(
+        dropDownValues[0]
+    );
     useEffect(() => {
         const fetchActivityHistory = async () => {
             try {
@@ -27,7 +36,7 @@ const UserActivityHistoryModal = ({ userId, isOpen, onRequestClose }) => {
     return (
         <Modal
             isOpen={isOpen}
-            className={`w-[820px] h-[600px] flex flex-col items-center justify-center 
+            className={`w-[1318.09px] h-[560.6px] flex flex-col items-center justify-center 
                 rounded-[20px] bg-neumorphicBg p-[10px]`}
             overlayClassName="modal-overlay"
             onRequestClose={onRequestClose}
@@ -35,16 +44,32 @@ const UserActivityHistoryModal = ({ userId, isOpen, onRequestClose }) => {
         >
             <div className="w-full h-full flex flex-col">
                 <div className="flex flex-row justify-between px-[5px]">
-                    <p className="font-dmSans text-[20px] font-medium">Activity History</p>
-                    <CustomCloseButton ClickFn={onRequestClose} />
+                    <div className="flex flex-row gap-[16.44px] items-center">
+                        <p className="font-dmSans text-[25px] font-semibold">
+                            Activity History
+                        </p>
+                        <Select
+                            value={selectedAttribute}
+                            onChange={setSelectedAttribute}
+                            options={dropDownValues}
+                            className={`w-[218px] max-sm:w-[110px] my-[2px] font-dmSans`}
+                            classNamePrefix="react-select"
+                            isSearchable={false}
+                        />
+                    </div>
+                    <IoClose className="size-[24px] cursor-pointer" />
                 </div>
 
                 {isLoading ? (
                     <LoadingItem size={10} />
                 ) : (
                     <div className="flex flex-col w-full h-[90%] mt-[15px] px-[10px] gap-[10px] overflow-y-auto">
-                        <UserActivityHistorySection sectionTitle={"Referrals Sent/Received"}/>
-                        <UserActivityHistorySection sectionTitle={"Premium Services"}/>
+                        <UserActivityHistorySection
+                            sectionTitle={"Referrals Sent/Received"}
+                        />
+                        <UserActivityHistorySection
+                            sectionTitle={"Premium Services"}
+                        />
                     </div>
                 )}
             </div>
